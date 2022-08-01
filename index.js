cd3e26d1d9b58d172fdf0859f1d6a0c04b5738dd
un-earthly <vijayalamin@gmail.com>
2022-08-01 09:03:30 -0700

    sidebar dynamic data on focus

--- index.js
+++ index.js
@@ -6,57 +6,74 @@ const toggle = (el, toAdd, ToRemove) => {
     el.classList.remove(ToRemove)
 }
 
-
+const taskcontainer = getElement('taskcontainer')
 const sidebarIcon = getElement('menuicon');
 const sidebar = getElement('sidebar');
+const ol = getElement('sidebar_task_heading')
+// genrating and showing sidebar information
+
+// toggling sidebar
 
 sidebarIcon.addEventListener('click', () => {
-    const isActive = sidebar.classList.contains('visible')
-    if (isActive) {
-        toggle(sidebar, 'not_visible', 'visible')
-    } else {
+    if (!sidebar.classList.contains('visible')) {
         toggle(sidebar, 'visible', 'not_visible')
+    } else {
+        toggle(sidebar, 'not_visible', 'visible')
     }
 })
 
-// genrating and showing sidebar information
-async function fetchPosts() {
-    const response = await fetch(`data.json`)
-    let data = await response.json()
-    show(data)
+
+
+// data display
+
+
+function fetchData() {
+    fetch('data.json')
+        .then(res => res.json())
+        .then(data => showData(data.tasks))
+        .catch(err => console.log(err))
 }
 
 
 function show(data) {
-    const ol = getElement('sidebar_task_heading')
     const li = createElement('li');
-    data.tasks.map(task => {
+    data.map(task => {
         li.innerHTML = task.task_title
         ol.appendChild(li)
         showNestedUl(task.assets, ol)
     })
 }
 
-function showNestedUl(data, ol) {
+function showNestedUl(data) {
     let ul = createElement('ul');
-    const taskcontainer = getElement('taskcontainer')
     data.map(asset => {
         let li = document.createElement('li');
         li.innerHTML = asset.asset_title
         ul.appendChild(li)
     })
-
-    data.map(task => {
-        const h3 = createElement('h3')
-        const p = createElement('p')
-        const div = createElement('div')
-        h3.innerHTML = task.asset_title
-        div.appendChild(h3)
-        div.appendChild(p)
-        console.log(task)
-        taskcontainer.appendChild(div)
-    })
     ol.appendChild(ul)
 }
 
-fetchPosts()
+
+function showData(data) {
+    observer.observe(sidebar, {
+        attributes: true
+    });
+    if (!sidebar.classList.contains('visible')) {
+        ol.innerHTML = ""
+        const borderedP = createElement('p')
+        borderedP.setAttribute('id', "borderedP")
+        borderedP.innerHTML = data.length;
+        ol.appendChild(borderedP)
+    }
+    else {
+        ol.innerHTML = ""
+        show(data)
+    }
+}
+fetchData()
+
+const observer = new MutationObserver(mutations => {
+    mutations.map(mutation => (mutation.attributeName === "class") && fetchData()
+    );
+})
